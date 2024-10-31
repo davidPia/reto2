593a90df797004bbda7fd3b7b4d9e3a878fda99d
dPiazuelo <DAM1@aula11-pc27>
2024-10-31 08:38:50 +0100

    Funciones1_Longitudes

--- reto2/src/retos/Funciones1.java
+++ reto2/src/retos/Funciones1.java
@@ -1,5 +1,21 @@
 package retos;
 
-public class Funciones1 {
+import java.util.Scanner;
 
+public class Funciones1 {
+	
+	
+	public static double Millas_Km(Scanner sc) {
+		String texto="Introduce la longitud en millas";
+		double millas=Funciones.dimeDouble(texto, sc);
+		double Km=millas*1.60934;
+		return Km;
+	}
+	
+	public static double Km_Millas(Scanner sc) {
+		String texto="Introduce la longitud en kilometros";
+		double Km=Funciones.dimeDouble(texto, sc);
+		double millas=Km/1.60934;
+		return millas;
+	}
 }
