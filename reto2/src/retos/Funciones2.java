806a692bfa2b109a3a16a8356d311ac81be1263e
ErnestoRe <ernestorezustaaznarez@gmail.com>
2024-10-31 08:38:56 +0100

    Funciones2_Temperaturas

--- reto2/src/retos/Funciones2.java
+++ reto2/src/retos/Funciones2.java
@@ -1,5 +1,18 @@
 package retos;
 
-public class Funciones2 {
+import java.util.Scanner;
 
+public class Funciones2 {
+	public static double Celsius_Fahrenheit( Scanner sc) {
+		String texto = "Introduce una temperatura en Celsius";
+		double cels = Funciones.dimeDouble(texto, sc);
+		double fahren = (cels*9/5)+32;
+		return fahren;
+	}
+	public static double Fahrenheit_Celsius( Scanner sc) {
+		String texto = "Introduce una temperatura en Fahrenheit";
+		double fahren = Funciones.dimeDouble(texto, sc);
+		double cels = (fahren-32)*5/9;
+		return cels;
+	}
 }
